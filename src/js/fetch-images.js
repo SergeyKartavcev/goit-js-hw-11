e559bab7a1d3a48cf12fa531c577c8d1ecede3c8
SergeyKartavcev <102188021+SergeyKartavcev@users.noreply.github.com>
2022-08-30 20:53:10 +0300

    first save

---
+++ src/js/fetch-images.js
@@ -0,0 +1,11 @@
+ import axios from "axios";
+ axios.defaults.baseURL = 'https://pixabay.com/api/';
+ const KEY = '29432649-8ecc53e09c3218583a5f8b5f1';
+
+ async function fetchImages(query, page, perPage){
+const response = await axios.get(`?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}&per_page=${perPage}`)
+return response
+
+}
+
+export { fetchImages }
